e391d05ad6d52a30240da324c4fe75569b9c8112
Rocky Truong <rockytruong19@gmail.com>
2021-05-19 11:47:13 -0700

    Completed javascript-forms.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,13 @@
+var $form = document.getElementById('contact-form');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var messageData = {
+    name: $form.elements.name.value,
+    email: $form.elements.email.value,
+    message: $form.elements.message.value
+  };
+  console.log('messageData:', messageData);
+}
+
+$form.addEventListener('submit', handleSubmit);
