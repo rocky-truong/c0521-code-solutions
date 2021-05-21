82edd76973477e4e3859c221843ba7739995f7a4
Rocky Truong <rockytruong19@gmail.com>
2021-05-21 00:22:19 -0700

    Updated varible and function names to be more descriptive.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,20 +1,20 @@
 var $openModalButton = document.querySelector('.open-modal-button');
 var $body = document.querySelector('.body');
 var $noButton = document.querySelector('.no-button');
-var $modal = document.querySelector('.modal');
+var $openModal = document.querySelector('.modal');
 
 function modalHandler(event) {
-  $modal.className = 'modal';
+  $openModal.className = 'modal';
   $body.className = 'body dimmed';
   $openModalButton.className = 'open-modal-button dimmer';
 }
 
 $openModalButton.addEventListener('click', modalHandler);
 
-function noHandler(event) {
-  $modal.className = 'modal hidden';
+function closeModal(event) {
+  $openModal.className = 'modal hidden';
   $body.className = 'body';
   $openModalButton.className = 'open-modal-button';
 }
 
-$noButton.addEventListener('click', noHandler);
+$noButton.addEventListener('click', closeModal);
