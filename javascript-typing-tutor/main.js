2b6b94e701f850cbff4ef45e302db692bd94a507
Rocky Truong <rockytruong19@gmail.com>
2021-05-24 02:54:02 -0700

    Completed javascript-typing-tutor.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,2 +1,14 @@
-/* var $span = document.querySelectorAll('span');
-*/
+var $letter = document.querySelectorAll('span');
+var currentLetter = 0;
+
+function keyHandler(event) {
+  if (event.key === $letter[currentLetter].textContent) {
+    $letter[currentLetter].className = 'letter correct';
+    $letter[currentLetter + 1].className = 'letter current';
+    currentLetter++;
+  } else {
+    $letter[currentLetter].className = 'letter incorrect';
+  }
+}
+
+document.addEventListener('keydown', keyHandler);
