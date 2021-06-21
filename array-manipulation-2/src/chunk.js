b8e47337014f1f1fc2b1449edef08366d0d83bd3
Rocky Truong <Rockytruong19@gmail.com>
2021-06-21 22:44:04 +0000

    In progress.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,15 +1,15 @@
 /* exported chunk */
-/*
-function chunk(array, size) {
-  if (array.length === 0) {
-    return [];
-  }
-  var firstArray = [];
-  var secondArray = [];
-  var count = 0;
-  for (var i = count; i < size; i++) {
-    firstArray.push(array[i]);
-    count += 1;
-  }
-}
-*/
+
+// function chunk(array, size) {
+//   if (array.length === 0) {
+//     return [];
+//   }
+//   var count = 0;
+//   var newArray = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (newArray[i].length === size) {
+//     newArray[i + 1].push([.push(array[i])]]);
+//     count += 1;
+//     }
+//   }
+// }
