2629c905d2d4b63ba66fb5adde60eee8f16465f8
Rocky Truong <Rockytruong19@gmail.com>
2021-06-23 07:25:26 +0000

    Completed array-manipulation-2.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,15 +1,22 @@
 /* exported chunk */
 
-// function chunk(array, size) {
-//   if (array.length === 0) {
-//     return [];
-//   }
-//   var count = 0;
-//   var newArray = [];
-//   for (var i = 0; i < array.length; i++) {
-//     if (newArray[i].length === size) {
-//     newArray[i + 1].push([.push(array[i])]]);
-//     count += 1;
-//     }
-//   }
-// }
+function chunk(array, size) {
+  if (array.length === 0) {
+    return [];
+  }
+  var count = 0;
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (i === 0) {
+      newArray.push([]);
+      newArray[count].push(array[i]);
+    } else if (newArray[count].length < size) {
+      newArray[count].push(array[i]);
+    } else if (newArray[count].length === size) {
+      count++;
+      newArray.push([]);
+      newArray[count].push(array[i]);
+    }
+  }
+  return newArray;
+}
