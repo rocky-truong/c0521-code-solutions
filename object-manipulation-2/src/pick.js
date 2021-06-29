593d8191c6302437d1730cca883b29c0d2ad1808
Rocky Truong <Rockytruong19@gmail.com>
2021-06-29 08:00:41 +0000

    In progress.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,10 @@
 /* exported pick */
+function pick(source, keys) {
+  var result = {};
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      result[keys[i]] = source[keys[i]];
+    }
+  }
+  return result;
+}
