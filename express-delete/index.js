50ed21ff6e09044b6eb8fc6c575e746b108daa88
Rocky Truong <Rockytruong19@gmail.com>
2021-06-29 00:22:12 +0000

    Completed express-delete.

---
+++ express-delete/index.js
@@ -0,0 +1,40 @@
+const express = require('./node_modules/express');
+const app = express();
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  const arrayGrades = [];
+  for (const key in grades) {
+    arrayGrades.push(grades[key]);
+  }
+  res.json(arrayGrades);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  delete grades[req.params.id];
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Port 3000 on!');
+});
