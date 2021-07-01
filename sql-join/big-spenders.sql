84104723a828da309a7041832233999708296356
Rocky Truong <Rockytruong19@gmail.com>
2021-07-01 08:47:54 +0000

    Completed sql-join.

---
+++ sql-join/big-spenders.sql
@@ -0,0 +1,8 @@
+select "c"."customerId",
+       "c"."firstName",
+       "c"."lastName",
+       "p"."amount"
+  from     "payments" as "p"
+  join     "customers" as "c" using ("customerId")
+  order by "p"."amount" desc
+  limit 10;
