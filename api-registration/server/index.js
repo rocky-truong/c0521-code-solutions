b42f10fca02a517f7f3d1fa7a4f4c629371edaa2
Rocky Truong <Rockytruong19@gmail.com>
2021-07-14 00:51:19 +0000

    Added another catch method at the end of the hash method.

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -38,7 +38,8 @@ app.post('/api/auth/sign-up', (req, res, next) => {
           res.status(201).json(newUser);
         })
         .catch(err => next(err));
-    });
+    })
+    .catch(err => next(err));
 
   /* your code starts here */
 
