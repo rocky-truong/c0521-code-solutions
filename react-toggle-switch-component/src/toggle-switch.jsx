77e3dc1195f931de73246a3d4a30a81b58437eda
Rocky Truong <Rockytruong19@gmail.com>
2021-07-21 23:07:59 +0000

    Completed react-toggle-switch-component.

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,1 +1,45 @@
 import React from 'react';
+
+class ToggleSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { isOn: false };
+  }
+
+  handleClick() {
+    if (!this.state.isOn) {
+      this.setState({ isOn: true });
+    } else {
+      this.setState({ isOn: false });
+    }
+  }
+
+  render() {
+    let div;
+    const isOn = this.state.isOn;
+    if (isOn) {
+      div = <div className="container">
+              <button onClick={this.handleClick} className="button-on">
+                <div className="oval">
+                  <div className="circle-on"></div>
+                </div>
+              </button>
+              <span>ON</span>
+            </div>;
+      return div;
+    } else {
+      div = <div className="container">
+              <button onClick={this.handleClick} className="button-off">
+                <div className="oval">
+                  <div className="circle-off"></div>
+                </div>
+              </button>
+              <span>OFF</span>
+            </div>;
+      return div;
+    }
+  }
+}
+
+export default ToggleSwitch;
