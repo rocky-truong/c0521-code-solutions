9ae393008ece844c8f0f9bdaeda28fc207b4f535
Rocky Truong <rockytruong19@gmail.com>
2021-10-16 16:59:34 -0700

    Finished 1/4.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,18 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  for (let i = 0; i < firstString.length; i++) {
+    if (secondString.includes(firstString[i])) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  // for (let j = 0; j < secondString.length; j++) {
+  //   if (firstString.includes(secondString[j])) {
+  //     continue;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  return true;
+}
