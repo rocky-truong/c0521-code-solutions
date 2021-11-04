f2d3c49ab973ca8a74e955f9511b7704caad4662
Rocky Truong <rockytruong19@gmail.com>
2021-11-04 03:10:46 -0700

    Completed data-structures-queues.

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,4 +1,16 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
+  if (queue.peek() === undefined) {
+    return undefined;
+  }
+  let counter = 0;
+  while (index >= counter) {
+    const topValue = queue.dequeue();
+    if (index === counter) {
+      return topValue;
+    }
+    counter++;
+    queue.enqueue(topValue);
+  }
 }
