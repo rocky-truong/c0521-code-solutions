e76eef0b2f9d37554f9666f5a296e39912f84085
Rocky Truong <rockytruong19@gmail.com>
2021-11-03 17:55:08 -0700

    In progress.

--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -1,5 +1,19 @@
 /* exported maxValue */
 
 function maxValue(stack) {
-
+  if (!stack.peek()) {
+    return -Infinity;
+  }
+  let currentValue = null;
+  let highestValue = null;
+  while (stack.peek() !== undefined) {
+    currentValue = stack.pop();
+    if (highestValue === null) {
+      highestValue = currentValue;
+    }
+    if (currentValue > highestValue) {
+      highestValue = currentValue;
+    }
+  }
+  return highestValue;
 }
